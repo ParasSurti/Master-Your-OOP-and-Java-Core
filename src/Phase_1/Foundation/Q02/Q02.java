b02b20c4c71ed8ca2c9139b9f04de73baadbef07
Paras Surti <paras16surti@gmail.com>
2026-08-07 21:02:47 +0530

    Q2.Create a parameterized constructor for Student that accepts name and age.

---
+++ src/Phase_1/Foundation/Q02/Q02.java
@@ -0,0 +1,10 @@
+// Create a parameterized constructor for Student that accepts name and age.
+package Phase_1.Foundation.Q02;
+
+public class Q02
+{
+    public static void main(String[] args)
+    {
+        Student constructor = new Student(21,"Paras");
+    }
+}
---
+++ src/Phase_1/Foundation/Q02/Student.java
@@ -0,0 +1,11 @@
+package Phase_1.Foundation.Q02;
+
+public class Student {
+    int fieldAge;
+    String fieldName;
+    public Student(int age, String name)
+    {
+        fieldAge = age;
+        fieldName = name;
+    }
+}
