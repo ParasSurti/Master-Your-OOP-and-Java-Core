f1def4ce27c681f2f20fa812894db307ed113408
Paras Surti <paras16surti@gmail.com>
2026-08-07 21:54:56 +0530

    Q3.Create a class Rectangle whose constructor takes length and breadth and prints the area.

---
+++ src/Phase_1/Foundation/Q03/Q03.java
@@ -0,0 +1,11 @@
+// Create a class Rectangle whose constructor takes length and breadth and prints the area.
+package Phase_1.Foundation.Q03;
+
+public class Q03
+{
+    public static void main(String[] args)
+    {
+        Rectangle rec = new Rectangle(2,4);
+        System.out.println(rec.area);
+    }
+}
---
+++ src/Phase_1/Foundation/Q03/Rectangle.java
@@ -0,0 +1,10 @@
+package Phase_1.Foundation.Q03;
+
+public class Rectangle
+{
+    int area ;
+    public Rectangle(int l, int b)
+    {
+        area = l * b;
+    }
+}
