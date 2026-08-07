ba5423ada23d2fc002412634dbd96a7ce5a579d1
Paras Surti <paras16surti@gmail.com>
2026-08-07 20:38:33 +0530

    Q1.Create a class Student with a default constructor that sets name to 'Unknown' and age to 0.

---
+++ src/Phase_1/Foundation/Q01/Q01.java
@@ -0,0 +1,12 @@
+//Create a class Student with a default constructor that sets name to “Unknown” and age to 0.
+package Phase_1.Foundation.Q01;
+
+public class Q01
+{
+    public static void main(String[] args)
+    {
+        Student cons = new Student();
+        System.out.println(cons.age);
+        System.out.println(cons.name);
+    }
+}
---
+++ src/Phase_1/Foundation/Q01/Student.java
@@ -0,0 +1,12 @@
+package Phase_1.Foundation.Q01;
+
+public class Student
+{
+    int age ;
+    String name;
+    public Student()
+    {
+         age = 0;
+         name = "Unknown";
+    }
+}
