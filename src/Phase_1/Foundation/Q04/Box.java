5e5f73bcc8d0ad779bc9e382db6711ab60ec8be7
Paras Surti <paras16surti@gmail.com>
2026-08-07 22:03:59 +0530

    Q4.Demonstrate constructor overloading in a class Box (no-arg, one-dimension, three-dimension).

---
+++ src/Phase_1/Foundation/Q04/Box.java
@@ -0,0 +1,18 @@
+package Phase_1.Foundation.Q04;
+
+public class Box
+{
+    int ans;
+    public Box()
+    {
+        System.out.println("No-Argument Constructor");
+    }
+    public Box(int a)
+    {
+        System.out.println("One Dimension Constructor");
+    }
+    public Box(int l, int w, int h)
+    {
+        System.out.println("Three Dimension Constructor");
+    }
+}
---
+++ src/Phase_1/Foundation/Q04/Q04.java
@@ -0,0 +1,12 @@
+//Demonstrate constructor overloading in a class Box (no-arg, one-dimension, three-dimension).
+package Phase_1.Foundation.Q04;
+
+import javax.swing.*;
+
+public class Q04
+{
+    public static void main(String[] args)
+    {
+        Box box = new Box(1,2,3);
+    }
+}
