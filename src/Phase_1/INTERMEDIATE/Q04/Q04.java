4f3044a328d440a12e92578dcac35da7db9bc150
Paras Surti <paras16surti@gmail.com>
2026-08-08 21:15:47 +0530

    Q4.Use this to return the current object from a method (method chaining) in a simple class.

---
+++ src/Phase_1/INTERMEDIATE/Q04/Q04.java
@@ -0,0 +1,11 @@
+//Use this to return the current object from a method (method chaining) in a simple class.
+package Phase_1.INTERMEDIATE.Q04;
+
+public class Q04
+{
+    public static void main(String[] args)
+    {
+        Simple simple = new Simple();
+        System.out.println();
+    }
+}
---
+++ src/Phase_1/INTERMEDIATE/Q04/Simple.java
@@ -0,0 +1,13 @@
+package Phase_1.INTERMEDIATE.Q04;
+
+public class Simple
+{
+    Simple method1()
+    {
+       return this;
+    }
+    void trigger()
+    {
+        method1();
+    }
+}
