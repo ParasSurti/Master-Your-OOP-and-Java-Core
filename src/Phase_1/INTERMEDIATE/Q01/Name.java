f14587b00acb5d155801d4e8639f7cc8c47844bb
Paras Surti <paras16surti@gmail.com>
2026-08-08 12:46:23 +0530

    Q1.Use this to resolve a naming conflict between an instance variable and a constructor parameter.

---
+++ src/Phase_1/INTERMEDIATE/Q01/Name.java
@@ -0,0 +1,10 @@
+package Phase_1.INTERMEDIATE.Q01;
+
+public class Name
+{
+    String name;
+    Name(String name)
+    {
+        this.name = name;
+    }
+}
---
+++ src/Phase_1/INTERMEDIATE/Q01/Q01.java
@@ -0,0 +1,11 @@
+//Use this to resolve a naming conflict between an instance variable and a constructor parameter.
+package Phase_1.INTERMEDIATE.Q01;
+
+public class Q01
+{
+    public static void main(String[] args) {
+        Name str = new Name("Paras");
+        System.out.println(str.name);
+    }
+
+}
