96462011a0f1af742d67a2b6bd02ff8cebdfd800
Paras Surti <paras16surti@gmail.com>
2026-08-08 11:23:14 +0530

    Q7.Show that a class loses its implicit default constructor once a parameterized constructor is added.

---
+++ src/Phase_1/Foundation/Q07/Q07.java
@@ -0,0 +1,11 @@
+// Show that a class loses its implicit default constructor once a parameterized constructor is added.
+package Phase_1.Foundation.Q07;
+
+public class Q07
+{
+    public static void main(String[] args)
+    {
+//        Proved that class loses its implicit default constructor once parameterized constructor is added
+//        parameter explicit = new parameter();
+    }
+}
---
+++ src/Phase_1/Foundation/Q07/parameter.java
@@ -0,0 +1,10 @@
+package Phase_1.Foundation.Q07;
+
+public class parameter
+{
+    int age ;
+    parameter(int age)
+    {
+        this.age = age;
+    }
+}
