c606a1a08faf797d5f3133fa5fa3bb87ae90e24c
Paras Surti <paras16surti@gmail.com>
2026-08-08 12:15:14 +0530

    Q10.Create a class Circle with radius; overload the constructor to also accept a diameter and derive radius from it.

---
+++ src/Phase_1/Foundation/Q10/Circle.java
@@ -0,0 +1,14 @@
+package Phase_1.Foundation.Q10;
+
+public class Circle
+{
+    float r;
+    Circle(float r)
+    {
+        this.r = r;
+    }
+    Circle(int d)
+    {
+        this.r = (float) d / 2;
+    }
+}
---
+++ src/Phase_1/Foundation/Q10/Q10.java
@@ -0,0 +1,11 @@
+//Create a class Circle with radius; overload the constructor to also accept a diameter and derive radius from it.
+package Phase_1.Foundation.Q10;
+
+public class Q10
+{
+    public static void main(String[] args)
+    {
+        Circle radius = new Circle(12);
+        System.out.println("The radius is " + radius.r);
+    }
+}
