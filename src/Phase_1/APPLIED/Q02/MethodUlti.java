81d617cb983a9c14c96e8f96a5b905818ba030f8
Paras Surti <paras16surti@gmail.com>
2026-08-09 22:48:55 +0530

    Q2.Write a static utility method that does not depend on any instance data (e.g., add two numbers).

---
+++ src/Phase_1/APPLIED/Q02/MethodUlti.java
@@ -0,0 +1,9 @@
+package Phase_1.APPLIED.Q02;
+
+public class MethodUlti
+{
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ src/Phase_1/APPLIED/Q02/Q02.java
@@ -0,0 +1,14 @@
+//Write a static utility method that does not depend on any instance data (e.g., add two numbers).
+package Phase_1.APPLIED.Q02;
+
+public class Q02
+{
+    public static void main(String[] args)
+    {
+        int a;
+        int b;
+        int add = MethodUlti.add(1,2);
+        System.out.println(add);
+    }
+
+}
