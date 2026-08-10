bf925167054d91d7a528860ee9f4f9c6a1c67b17
Paras Surti <paras16surti@gmail.com>
2026-08-10 19:56:22 +0530

    Q7.Create a utility class MathUtils with only static methods and a private constructor.

---
+++ src/Phase_1/APPLIED/Q07/MathUtils.java
@@ -0,0 +1,17 @@
+package Phase_1.APPLIED.Q07;
+
+public class MathUtils
+{
+    static int a ;
+    static int b ;
+    private MathUtils()
+    {
+
+    }
+    static int add(int a,int b)
+    {
+        return a + b;
+    }
+}
+
+
---
+++ src/Phase_1/APPLIED/Q07/Q07.java
@@ -0,0 +1,11 @@
+//Create a utility class MathUtils with only static methods and a private constructor.
+package Phase_1.APPLIED.Q07;
+
+public class Q07
+{
+    public static void main(String[] args)
+    {
+//        MathUtils mathUtils = new MathUtils();
+        System.out.println(MathUtils.add(5,6));
+    }
+}
