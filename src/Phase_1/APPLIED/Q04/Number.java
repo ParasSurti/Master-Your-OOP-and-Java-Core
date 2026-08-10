b9ddf745d99425f702d5697e28b2fb6b2e9df20c
Paras Surti <paras16surti@gmail.com>
2026-08-10 13:00:45 +0530

    Q4.Create a static block that initializes a static variable once when the class is loaded.

---
+++ src/Phase_1/APPLIED/Q04/Number.java
@@ -0,0 +1,11 @@
+package Phase_1.APPLIED.Q04;
+
+public class Number
+{
+    static int num;
+    static
+    {
+        num = 10;
+        System.out.println("this static has block run");
+    }
+}
---
+++ src/Phase_1/APPLIED/Q04/Q04.java
@@ -0,0 +1,11 @@
+//Create a static block that initializes a static variable once when the class is loaded.
+package Phase_1.APPLIED.Q04;
+
+public class Q04
+{
+    public static void main(String[] args)
+    {
+        Number number;
+        System.out.println(Number.num);
+    }
+}
