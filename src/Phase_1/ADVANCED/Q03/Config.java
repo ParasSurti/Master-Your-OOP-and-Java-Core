0c027f80f8510a50f65e0d49f1dbd10994b14f58
Paras Surti <paras16surti@gmail.com>
2026-08-14 11:55:38 +0530

    Q3.Create a class Config holding static final constants for application-wide settings.

---
+++ src/Phase_1/ADVANCED/Q03/Config.java
@@ -0,0 +1,6 @@
+package Phase_1.ADVANCED.Q03;
+
+public class Config
+{
+    public static final String APP_NAME = "Build_It";
+}
---
+++ src/Phase_1/ADVANCED/Q03/Q03.java
@@ -0,0 +1,14 @@
+//Create a class Config holding static final constants for application-wide settings.
+package Phase_1.ADVANCED.Q03;
+
+public class Q03
+{
+    public static void main(String[] args)
+    {
+//        ERROR MSG : java: cannot assign a value to static final variable APP_NAME
+//        Config.APP_NAME = "PARAS";
+//        System.out.println(Config.APP_NAME);
+
+        System.out.println(Config.APP_NAME);
+    }
+}
